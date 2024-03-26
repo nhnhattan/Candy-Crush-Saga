9bce1638850e3bc20c3112fcb805b6e6da4afb5a
unknown <nhoxcon029@gmail.com>
2024-03-26 18:16:32 +0700

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { createBoard } from "./utils/createBoard";
+import { moveBelow, updateBoard } from "./store";
+import Board from "./components/Board";
+import {
+  checkForColumnOfThree,
+  checkForRowOfFour,
+  checkForRowOfThree,
+  isColumnOfFour,
+} from "./utils/moveCheckLogic";
+import {
+  formulaForColumnOfFour,
+  formulaForColumnOfThree,
+  generateInvalidMoves,
+} from "./utils/formulas";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+
+  const boardSize = useAppSelector(
+    ({ candyCrush: { boardSize } }) => boardSize
+  );
+
+  useEffect(() => {
+    dispatch(updateBoard(createBoard(boardSize)));
+  }, [dispatch, boardSize]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(
+        newBoard,
+        boardSize,
+        generateInvalidMoves(boardSize, true)
+      );
+      checkForColumnOfThree(
+        newBoard,
+        boardSize,
+        formulaForColumnOfThree(boardSize)
+      );
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard));
+      dispatch(moveBelow());
+    }, 150);
+    return () => clearInterval(timeout);
+  }, [board, dispatch, boardSize]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen">
+      <Board />
     </div>
   );
 }
